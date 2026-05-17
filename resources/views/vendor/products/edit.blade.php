cb4788f83653641fb71e1d94a266dd1d8df01399
en6113 <mitayan3@gmail.com>
2026-05-17 12:41:02 +0900

    productのbladeに権限チェック(@can)を追加しました

--- resources/views/vendor/products/edit.blade.php
+++ resources/views/vendor/products/edit.blade.php
@@ -100,9 +100,11 @@
 
             {{-- 更新ボタン --}}
             <div class="flex space-x-4">
-                <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded">
-                    更新する
-                </button>
+                @can('update',$product)
+                    <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded">
+                        更新する
+                    </button>
+                @endcan
                 <a href="{{ route('products.index') }}"
                     class="bg-gray-500 hover:bg-gray-600 text-white px-4 py-2 rounded">
                     キャンセル
